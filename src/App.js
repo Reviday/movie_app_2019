d7ae9c933347759820c45f54f32d3e12f96d7372
Seong-Kyu Yun <reviday@naver.com>
2020-01-06 14:57:56 +0900

    #3.1 All you need to know about State

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ class App extends React.Component { // react는 자동적으로 너의 class com
   state = { // 너가 바꿀 데이터를 넣어
     count : 0
   };
-  add = () => {
-    console.log("add");
+  add = () => { // render function이 호출되도록 코딩해야함
+    this.setState(current => ({count: current.count + 1})) // setState를 호출하면 refresh를 함.
+                // 이것이 여러분이 state를 set할 때, 
+                // react에서 외부의 상태에 의존하지 않는 가장 좋은 방법
   };
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({count: current.count - 1}))
   };
+  // 너가 setState를 호출할 때마다 react는 다시 render 할거야.
   render() {
   return <div>
     <h1>The number is: {this.state.count}</h1>
