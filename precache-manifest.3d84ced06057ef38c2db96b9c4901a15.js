cbccc6411e2726ab78f9c57f38b50e9b456835d4
Seong-Kyu Yun <reviday@naver.com>
2020-01-06 22:10:45 +0900

    Updates

---
+++ precache-manifest.3d84ced06057ef38c2db96b9c4901a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c659424958ecd866b88ba0c526c1ac2e",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "22ec3d67e008da6e61ff",
+    "url": "/movie_app_2019/static/css/main.58108d44.chunk.css"
+  },
+  {
+    "revision": "1177a1d856be3feec0d5",
+    "url": "/movie_app_2019/static/js/2.16cf26ad.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.16cf26ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22ec3d67e008da6e61ff",
+    "url": "/movie_app_2019/static/js/main.3d0e9f06.chunk.js"
+  },
+  {
+    "revision": "efc8b6c6df8a6d5d9b7d",
+    "url": "/movie_app_2019/static/js/runtime-main.32955f5d.js"
+  }
+]);
